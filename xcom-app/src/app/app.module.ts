b3f47d4b7559fe173cacafd342b17b52a79fcbf3
Addison Snyder <ajsnyde@bgsu.edu>
2017-11-04 12:16:59 -0400

    Added menu, worked on routing.

--- xcom-app/src/app/app.module.ts
+++ xcom-app/src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WorldMap } from "./world-map/world-map.component";
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import {
+  RouterModule,
+  Routes,
+} from '@angular/router';
+
+const routes: Routes = [
+  { path: 'home', component: WorldMap },
+  { path: 'login', component: TopMenuComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    WorldMap
+    WorldMap,
+    TopMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, RouterModule.forRoot(routes)
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- xcom-app/src/app/world-map/world-map.component.ts
+++ xcom-app/src/app/world-map/world-map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { MapObject } from "./world-map.model";
-import { Observable } from 'rxjs/Rx';
+import { NgModel } from '@angular/forms';
+import { MapObject, AlienBase, XcomBase, XcomAircraft } from "./world-map.model";
+import { Observable, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'world-map',
@@ -11,11 +12,28 @@ export class WorldMap implements OnInit {
   objects: MapObject[] = [];
   constructor() {
   }
+  speed = 15;
+  height = 1000;
+  width = 2000;
+  tick: Subscription;
 
   ngOnInit() {
     for (let i = 0; i < 100; i++)
       this.objects.push(new MapObject());
-    Observable.interval(50).subscribe(x => {
+    for (let i = 0; i < 5; i++)
+      this.objects.push(new AlienBase());
+    for (let i = 0; i < 7; i++)
+      this.objects.push(new XcomBase());
+    for (let i = 0; i < 22; i++)
+      this.objects.push(new XcomAircraft());
+    this.timeChange();
+  }
+
+  timeChange() {
+    if (!!this.tick)
+      this.tick.unsubscribe();
+    console.log("time changed" + this.speed)
+    this.tick = Observable.interval((1000 * 15) / this.speed).subscribe(x => {
       this.move();
     });
   }
